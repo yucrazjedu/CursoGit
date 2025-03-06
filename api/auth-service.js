52b0ddad67b27b5dc5185e3b79377cb29031c8a5
yucrazj <yucrazj@gmail.com>
2025-03-05 20:23:00 -0400

    Funcion de cierre de sesion

--- api/auth-service.js
+++ api/auth-service.js
@@ -1,14 +1,20 @@
 async function login(user_name, password) {
   const response = await fetch("http://localhost:3000/login", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ user_name, password }),
-    });
-    const data = await response.json();
-    if (data.error) {
-        throw new Error(data.error);
-    }
-    return data;
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ user_name, password }),
+  });
+  const data = await response.json();
+  if (data.error) {
+    throw new Error(data.error);
+  }
+  return data;
+}
+
+function logout() {
+  localStorage.removeItem("token");
+  localStorage.removeItem("user_name");
+  window.location.href = "/index.html";
 }
